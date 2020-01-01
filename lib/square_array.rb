40a90099f4ae7d6b87a09f057419c80eaf60a79c
Adit P Patel <app2116@gmail.com>
2020-01-01 03:45:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   my_array = Array.new()
   while array[counter] do
-    my_array<<(array[counter}**2)
+    my_array<<(array[counter]**2)
 end
 return my_array
 end
